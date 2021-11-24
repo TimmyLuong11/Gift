524fe3e55cf8ad665c0bfee9de9d8a173a66ae4e
TimmyLuong11 <Timothy.luong@ou.edu>
2021-11-24 00:53:39 -0600

    Working Code

---
+++ Surprise/Surprise/BooAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surprise
+{
+    public class BooAPI
+    {
+        public string subject { get; set; }
+        public List<BooMessage> messageSelect { get; set; }
+
+        public override string ToString()
+        {
+            return subject;
+        }
+    }
+    public class BooMessage
+    {
+        public string message { get; set; }
+        public string img { get; set; }
+        public string desc { get; set; }
+
+        public override string ToString()
+        {
+            return message;
+        }
+    }
+}
---
+++ Surprise/Surprise/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Surprise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<BooAPI> booboo = new List<BooAPI>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            string jFile = File.ReadAllText("BooJSON.json");
+            List<BooAPI> json = JsonConvert.DeserializeObject<List<BooAPI>>(jFile);
+
+            foreach (var item in json)
+            {
+                booboo.Add(item);
+            }
+
+            PopulateComboBox();
+        }
+
+        private void PopulateComboBox()
+        {
+            combo.SelectedIndex = 0;
+            foreach (var item in booboo)
+            {
+                combo.Items.Add(item);
+            }
+        }
+
+        private void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BooAPI jam = (BooAPI)combo.SelectedItem;
+            ListBox.Items.Clear();
+            foreach (var item in jam.messageSelect)
+            {
+                ListBox.Items.Add(item);
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BooMessage selected = (BooMessage)ListBox.SelectedItem;
+            window win = new window();
+            win.SetupWindow(selected);
+            win.ShowDialog();
+        }
+    }
+}
